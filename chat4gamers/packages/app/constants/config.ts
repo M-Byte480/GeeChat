bf06341b76179e1f365fa2795935beb6099ae094
Milan <kmilanm13@gmail.com>
2026-02-25 19:25:49 +0000

    automatic update works

--- chat4gamers/packages/app/constants/config.ts
+++ chat4gamers/packages/app/constants/config.ts
@@ -1,3 +1,3 @@
-export const SERVER_IP =  'REDACTED_DOMAIN' // 'REDACTED_IP'; // 'localhost'
+export const SERVER_IP = 'REDACTED_DOMAIN' // 'REDACTED_IP'; // 'localhost' //   // 'REDACTED_IP'
 export const API_BASE = `https://${SERVER_IP}`; // :4000
 export const LIVEKIT_WS = `wss://${SERVER_IP}`; //:7880
